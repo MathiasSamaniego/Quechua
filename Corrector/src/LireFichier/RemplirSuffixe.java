50b2bd3866de7e18c4bba1d63fc507f073da50d6
Alain-SARKISIAN <alain.sarkisian@etu.u-pec.fr>
2020-06-04 18:30:22 +0200

    Suffixe

--- Corrector/src/LireFichier/RemplirSuffixe.java
+++ Corrector/src/LireFichier/RemplirSuffixe.java
@@ -5,19 +5,19 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 public class RemplirSuffixe {
-	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
+	
+	
+	public void RemplirSuffixes(String cheminFichier,String table) throws IOException, ClassNotFoundException, SQLException {
 		String chaine="";
 		int i = 0;
 		int j = 0;
 		int nombredelignesTotales = 0;
-		int nombredelignes = 0;
 	      //lecture du fichier texte
-	      //try{
-	         InputStream ips = new FileInputStream("src/LireFichier/Sufijos Adjectivaux QU.txt");
+	         InputStream ips = new FileInputStream(cheminFichier);
 	         InputStreamReader in = new InputStreamReader(ips);
 	         BufferedReader br = new BufferedReader(in);
 	         String ligne;
@@ -29,46 +29,37 @@ public class RemplirSuffixe {
 	            } else { 
 	            	i = 0;
 	            	j = 0;
-	            	while(ligne.charAt(i) != '"') {
+	            	while(ligne.charAt(i) != '/') {
+            			chaine = chaine + ligne.charAt(j);
 	            		i++;
 	            		j = i;
 	            	}
 	            	j++;
-		            if(ligne.charAt(j) == 'k') {
-	            		nombredelignes++;
-	            		while(ligne.charAt(j) != '"') {
-		            		if(ligne.charAt(j) == '(') break;
-	            			chaine = chaine + ligne.charAt(j);
-		            		j++;
-		            	}
-
 	            		System.out.println(chaine);
-	            	/*	Class.forName("org.sqlite.JDBC"); // loaded the driver (use properties)
+	            		Class.forName("org.sqlite.JDBC"); // loaded the driver (use properties)
 	            		String url = "jdbc:sqlite:C:\\Users\\alain\\OneDrive\\Documents/CorrectorQuechua.db";
 	            		//String url = "jdbc:sqlite://127.0.0.1:5432/DataBaseQuechua.db";
-	            		Connection c = null;
+	            		java.sql.Connection c = null;
 	            		c = DriverManager.getConnection(url);
-	    				Statement s = c.createStatement();
-	    				ResultSet st = s.executeQuery("SELECT * FROM A;");
-	    				boolean b = false;
-	    				while(st.next()) {
-	    					if(st.getString(2).equals(chaine) && st.getString(3).equals(type)) {
-	    						b = true;
-	    					}
-	    				}
-	    				if(b == false) {
-	    					System.out.println("INSERT INTO h (mot, type) VALUES('" + chaine + "','" + type + "');");
-	    					if(type.equals("MWE")) {	
-	    						s.executeUpdate("INSERT INTO h (mot, type) VALUES('" + chaine + "','MC');");
-	    					} else {
-	    						s.executeUpdate("INSERT INTO h (mot, type) VALUES('" + chaine + "','" + type + "');");
-	    					}
-	    				}*/
-		            }
+	    				java.sql.Statement s =  c.createStatement();
+	    					System.out.println("INSERT INTO "+ table +"(suffixe) VALUES('" + chaine +");");
+    						s.executeUpdate("INSERT INTO " + table +"(suffixe) VALUES('" + chaine + "');");
 	            }
 	         }
 	         System.out.println("Lignes totales : " + nombredelignesTotales);
-	         System.out.println("Lignes pour la lettre : " + nombredelignes);
 	         br.close();
 	      }
+
+	
+
+	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
+		RemplirSuffixe suffixe = new RemplirSuffixe();
+		suffixe.RemplirSuffixes("C:\\Users\\alain\\git\\Quechua\\Corrector\\src\\LireFichier\\Sufijos Adjectivaux  QU.txt","Suffixes_Adjectivaux");
+		suffixe.RemplirSuffixes("C:\\Users\\alain\\git\\Quechua\\Corrector\\src\\LireFichier\\Sufijos Adverbiaux  QU.txt","Suffixes_Averbiaux");
+		suffixe.RemplirSuffixes("C:\\Users\\alain\\git\\Quechua\\Corrector\\src\\LireFichier\\Sufijos Nominaux  QU.txt","Suffixes_Nominaux");
+		suffixe.RemplirSuffixes("C:\\Users\\alain\\git\\Quechua\\Corrector\\src\\LireFichier\\Sufijos pos_posés SPP.txt","Suffixes_PosPoses");
+		suffixe.RemplirSuffixes("C:\\Users\\alain\\git\\Quechua\\Corrector\\src\\LireFichier\\Sufijos Pronominaux  QU.txt","Suffixes_Pronomiaux");
+		suffixe.RemplirSuffixes("C:\\Users\\alain\\git\\Quechua\\Corrector\\src\\LireFichier\\Sufijos Verbaux inter-position QU.txt","Suffixes_VerbauxInterPosition");
+
+	}
 }
