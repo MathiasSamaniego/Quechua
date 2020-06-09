a7e6fed6b912cbf8f541c940804a39cbdbd9b3ee
sakao-town <pds_ing1_sakao@outlook.fr>
2020-06-09 16:31:09 +0200

    Refresh 2

--- ServerQuechua/src/RequetesBdd/Crud_Controller.java
+++ ServerQuechua/src/RequetesBdd/Crud_Controller.java
@@ -1,9 +1,28 @@
 package RequetesBdd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class Crud_Controller {
 	
 	
 	public Crud_Controller() throws ClassNotFoundException{
 	}
 	
+	
+	public ArrayList<String> showCorrection() throws ClassNotFoundException, SQLException {
+		ArrayList<String> retour = new ArrayList<String>();
+		Class.forName("org.sqlite.JDBC"); // loaded the driver (use properties)
+		String url = "jdbc:sqlite:C:\\Users\\mathi\\Documents\\StageQuechua/DataBaseQuechua.db";
+		// String url = "jdbc:sqlite://127.0.0.1:5432/DataBaseQuechua.db"; Connection
+		Connection c = null;
+		c = DriverManager.getConnection(url);
+		Statement s = c.createStatement();
+		ResultSet st = s.executeQuery("SELECT * FROM  ;");
+		return retour;
+	}
 }
